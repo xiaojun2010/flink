96b1f4e10fb1e7c8150a85834bd313eddbea98ad
xiaojun2010 <404869979@qq.com>
2024-07-22 14:22:50 +0800

    Changes

--- prometheus_demo/src/main/java/com/prometheus/demo/demo/constant/JobMetrics.java
+++ prometheus_demo/src/main/java/com/prometheus/demo/demo/constant/JobMetrics.java
@@ -2,8 +2,11 @@ package com.prometheus.demo.demo.constant;
 
 import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.MeterBinder;
+import io.micrometer.core.instrument.config.MeterFilter;
+import io.micrometer.core.instrument.distribution.DistributionStatisticConfig;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -16,12 +19,36 @@ public class JobMetrics implements MeterBinder {
 
     public Map<String, Double> map;
 
+//    private final Counter httpRequestsTotal = Counter.build()
+//            .name("http_requests_total")
+//            .help("Total number of http requests by response status code")
+//            .labelNames("endpoint", "status")
+//            .register();
+//
+//    private final Histogram httpRequestDurationMs = Histogram.build()
+//            .name("http_request_duration_milliseconds")
+//            .help("Http request latency histogram")
+//            .exponentialBuckets(25, 2, 7)
+//            .labelNames("endpoint", "status")
+//            .register();
+
     JobMetrics() {
         map = new HashMap<>();
     }
 
     @Override
     public void bindTo(MeterRegistry meterRegistry) {
+        meterRegistry.config()
+                .meterFilter(new MeterFilter() {
+                    @Override
+                    public DistributionStatisticConfig configure(Meter.Id id, DistributionStatisticConfig config) {
+                        return DistributionStatisticConfig.builder()
+                                .percentiles(0.95, 0.9, 0.5)
+                                .build()
+                                .merge(config);
+                    }
+                });
+
 
 
         //
